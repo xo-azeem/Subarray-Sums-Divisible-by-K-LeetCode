31bf6221572548625c43ba4db5391dd3b5e5ff1e
Muhammad Azeem <mailmeatazeem@gmail.com>
2024-06-24 15:09:26 +0500

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,33 @@
+// Code
+
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) 
+    {
+        int count = 0;
+        int prefixSum = 0;
+        std::unordered_map<int, int> prefixMap;
+        prefixMap[0] = 1;
+
+        for (int num : nums) 
+        {
+
+            prefixSum += num;
+
+            int mod = prefixSum % k;
+            
+            if (mod < 0)
+                mod += k;
+            
+            if (prefixMap.find(mod) != prefixMap.end()) 
+            {
+                count += prefixMap[mod];
+                prefixMap[mod] += 1;
+            }
+            else
+                prefixMap[mod] = 1;
+        }
+        
+        return count;
+    }
+};
